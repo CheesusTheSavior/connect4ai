871807f55d60a45447733d4f0b7936a04c13ec71
CheesusTheSavior <72360695+CheesusTheSavior@users.noreply.github.com>
2022-02-11 00:15:00 +0100

    added functions for bot tournament, score calculation and procreation

--- connect4.py
+++ connect4.py
@@ -2,264 +2,262 @@ import numpy as np
 import pandas as pd
 from itertools import cycle
 
+
 class ConnectFour:
-  
-  def __init__(self, name=None, rows=6, cols=7):
-    if name:
-      self.name=name
 
-    self.game_rows = range(1,rows+1)
-    self.game_cols = range(1,cols+1)
-    self.game = pd.DataFrame(data="",
-                            index=self.game_rows, 
-                            columns=self.game_cols)
+    def __init__(self, name=None, rows=6, cols=7):
+        if name:
+            self.name = name
 
-    self.turns = cycle([1,2])
-    self.turn = next(self.turns)
-    self.symbols = ["x","o"]
-    self.status = "start"
-    self.end = False
+        self.game_rows = range(1, rows + 1)
+        self.game_cols = range(1, cols + 1)
+        self.game = pd.DataFrame(data="",
+                                 index=self.game_rows,
+                                 columns=self.game_cols)
 
-  def show(self):
-    with pd.option_context('display.max_rows', 
-    None, 'display.max_columns', None): 
-      print(self.game)
+        self.turns = cycle([1, 2])
+        self.turn = next(self.turns)
+        self.symbols = ["x", "o"]
+        self.status = "start"
+        self.end = False
 
-  def take_turn(self):
-    first_input = True
-    valid = False
-    cur_player = self.player_names[self.turn-1]
-    while valid == False:
-      # take input from player (which column)
-      if first_input:
-        plays = input(f"{cur_player}, it is your turn!\n Choose your column:")
-        first_input = False
-      else:
-        plays = input(f"{cur_player}, this move is not valid. Choose a different column:")
-      if plays == "stop":
-        return plays
-      try:
-        plays = int(plays)
-      except ValueError:
-        print("This is not a valid input. Input an integer.")
-        continue
-      # check if player's input is correct
-      valid = True
-      # 1. must be inside range of columns
-      if plays not in self.game_cols:
+    def show(self):
+        with pd.option_context('display.max_rows',
+                               None, 'display.max_columns', None):
+            print(self.game)
+
+    def take_turn(self):
+        first_input = True
         valid = False
-        print("This column is not in range.")
-        continue
-      # 2. columns must not be full
-      if not "" in list(self.game[plays]):
-        valid = False
-        print("This column is full.")
-        continue
+        cur_player = self.player_names[self.turn - 1]
+        while valid == False:
+            # take input from player (which column)
+            if first_input:
+                plays = input(f"{cur_player}, it is your turn!\n Choose your column:")
+                first_input = False
+            else:
+                plays = input(f"{cur_player}, this move is not valid. Choose a different column:")
+            if plays == "stop":
+                return plays
+            try:
+                plays = int(plays)
+            except ValueError:
+                print("This is not a valid input. Input an integer.")
+                continue
+            # check if player's input is correct
+            valid = True
+            # 1. must be inside range of columns
+            if plays not in self.game_cols:
+                valid = False
+                print("This column is not in range.")
+                continue
+            # 2. columns must not be full
+            if not "" in list(self.game[plays]):
+                valid = False
+                print("This column is full.")
+                continue
 
-    # depending on player, drop a character in the lowest empty row
-    play_col = list(self.game[plays])
+        # depending on player, drop a character in the lowest empty row
+        play_col = list(self.game[plays])
 
-    done = False
-    i = -1
-    symbol = self.symbols[self.turn-1]
-    while not done:
-      if play_col[i] == "":
-        play_col[i] = symbol
-        self.game[plays] = play_col
-        print(f"\nPlaced a {symbol} in column {plays}.\n")
-        done = True
+        done = False
+        i = -1
+        symbol = self.symbols[self.turn - 1]
+        while not done:
+            if play_col[i] == "":
+                play_col[i] = symbol
+                self.game[plays] = play_col
+                print(f"\nPlaced a {symbol} in column {plays}.\n")
+                done = True
 
-        return "play"
-      else:
-        i -= 1
+                return "play"
+            else:
+                i -= 1
 
-  def check_winner(self):
-    # check if there are four identical pieces in any
-    # row, column or diagonal right next to each other
-    # check whether the game has ended in a tie
-    full = np.all(self.game.values != "")
-    if full:
-      self.status = "tie"
-      return True
+    def check_winner(self):
+        # check if there are four identical pieces in any
+        # row, column or diagonal right next to each other
+        # check whether the game has ended in a tie
+        full = np.all(self.game.values != "")
+        if full:
+            self.status = "tie"
+            return True
 
-    # iterate through all cells
+        # iterate through all cells
 
-    for col in self.game_cols:
-      for row in self.game_rows:
-        cell_val = self.game.loc[row,col]
-        if cell_val == "":
-          continue
-        right_cols = list(range(col, col+4))
-        lower_rows = list(range(row, row+4))
-        right_cells, lower_cells, diagonal_cells, diagonal_cells2 = ([None], [None], [None], [None])
-        if col+3 in self.game_cols:
-          right_cells = list(self.game.loc[row, right_cols])
-        if row+3 in self.game_rows:
-          lower_cells = list(self.game.loc[lower_rows, col])
-        if col+3 in self.game_cols and row+3 in self.game_rows:
-          diagonal_cells = []
-          for i in range(4):
-            diagonal_cells.append(self.game.at[row+i, col+i])
-        if col+3 in self.game_cols and row-3 in self.game_rows:
-          diagonal_cells2 = []
-          for i in range(4):
-            diagonal_cells2.append(self.game.at[row-i, col+i])
+        for col in self.game_cols:
+            for row in self.game_rows:
+                cell_val = self.game.loc[row, col]
+                if cell_val == "":
+                    continue
+                right_cols = list(range(col, col + 4))
+                lower_rows = list(range(row, row + 4))
+                right_cells, lower_cells, diagonal_cells, diagonal_cells2 = ([None], [None], [None], [None])
+                if col + 3 in self.game_cols:
+                    right_cells = list(self.game.loc[row, right_cols])
+                if row + 3 in self.game_rows:
+                    lower_cells = list(self.game.loc[lower_rows, col])
+                if col + 3 in self.game_cols and row + 3 in self.game_rows:
+                    diagonal_cells = []
+                    for i in range(4):
+                        diagonal_cells.append(self.game.at[row + i, col + i])
+                if col + 3 in self.game_cols and row - 3 in self.game_rows:
+                    diagonal_cells2 = []
+                    for i in range(4):
+                        diagonal_cells2.append(self.game.at[row - i, col + i])
 
-        # check if the same symbol is in the next (right, lower or diagonal) three cells
-        right_4, lower_4, diag_4, diag2_4 = (None, None, None, None)
-        if right_cells:
-          right_4 = [True if x==cell_val else False for x in right_cells]
-        if lower_cells:
-          lower_4 = [True if x==cell_val else False for x in lower_cells]
-        if diagonal_cells:
-          diag_4 = [True if x==cell_val else False for x in diagonal_cells]
-        if diagonal_cells2:
-          diag2_4 = [True if x==cell_val else False for x in diagonal_cells2]
+                # check if the same symbol is in the next (right, lower or diagonal) three cells
+                right_4, lower_4, diag_4, diag2_4 = (None, None, None, None)
+                if right_cells:
+                    right_4 = [True if x == cell_val else False for x in right_cells]
+                if lower_cells:
+                    lower_4 = [True if x == cell_val else False for x in lower_cells]
+                if diagonal_cells:
+                    diag_4 = [True if x == cell_val else False for x in diagonal_cells]
+                if diagonal_cells2:
+                    diag2_4 = [True if x == cell_val else False for x in diagonal_cells2]
 
-        if all(right_4) or all(lower_4) or all(diag_4) or all(diag2_4):
-          self.winner = self.symbols.index(cell_val)+1
-          self.status = "finished"
-          return True
+                if all(right_4) or all(lower_4) or all(diag_4) or all(diag2_4):
+                    self.winner = self.symbols.index(cell_val) + 1
+                    self.status = "finished"
+                    return True
 
-    # if that is true, set winner and return true
-    return False
+        # if that is true, set winner and return true
+        return False
 
-  def play(self):
-    command = None
-    print("We are now playing CONNECT 4!!!")
-    self.show()
-    if not hasattr(self, "player_names"):
-      pl1_name = input("Player 1, what is your name?\n")
-      pl2_name = input("Player 2, what is your name?\n")
-      self.player_names = [pl1_name, pl2_name]
-    while self.check_winner() == False and command !="stop":
-      command = self.take_turn()
-      self.convert()
-      self.show()
+    def play(self):
+        command = None
+        print("We are now playing CONNECT 4!!!")
+        self.show()
+        if not hasattr(self, "player_names"):
+            pl1_name = input("Player 1, what is your name?\n")
+            pl2_name = input("Player 2, what is your name?\n")
+            self.player_names = [pl1_name, pl2_name]
+        while self.check_winner() == False and command != "stop":
+            command = self.take_turn()
+            self.convert()
+            self.show()
 
-
-    if hasattr(self, "winner"):
-      print(f"The winner is Player {self.winner}!")
-    elif self.status == "tie":
-      print(f"The game has ended in a tie!")
-    else:
-      print(f"No winner yet.")
-
-  def reset(self):
-    self.game = pd.DataFrame(data="",
-                            index=self.game_rows, 
-                            columns=self.game_cols)
-    if hasattr(self, "winner"):
-      del(self.winner)
-    del(self.player_names)
-    self.turn = next(self.turns)
-    self.status = "start"
-
-  def convert(self):
-    game = self.game.values
-    shape = game.shape
-    converted_game = np.zeros(shape)
-    for x in range(0, shape[0]):
-      for y in range(0, shape[1]):
-        if game[x,y] == "":
-          converted_game[x,y] = 0
-        elif game[x,y] == "x":
-          converted_game[x,y] = 1
-        elif game[x,y] == "o":
-          converted_game[x,y] = -1
-    self.conv_game = converted_game.reshape(42,1)
-
-  def play_vs_ai(self, bot):
-    command = None
-    print("We are now playing CONNECT 4!!!")
-    self.show()
-    if not hasattr(self, "player_names"):
-      pl1_name = input("Player 1, what is your name?\n")
-      pl2_name = bot.name
-      self.player_names = [pl1_name, pl2_name]
-    while self.check_winner() == False and command !="stop":
-      if self.turn == 1:
-        command = self.take_turn()
-      else:
-        self.bot_turn(bot)
-
-      self.convert()
-      self.show()
-      self.turn = next(self.turns)
-
-
-    if hasattr(self, "winner"):
-      print(f"The winner is {self.winner}!")
-    elif self.status == "tie":
-      print(f"The game has ended in a tie!")
-    else:
-      print(f"No winner yet.")
-
-  def bot_turn(self, bot):
-    game_inputs = self.conv_game
-    if self.turn == 2:
-      game_inputs = [-x for x in game_inputs]
-    # let the bot calculate his turn
-    bot_activation = list(bot.calculate(game_inputs))
-    # play the column with the highest amplitude
-    print(bot_activation)
-    bot_plays = [int(bot_activation.index(x))+1 for x in sorted(bot_activation, reverse=True)]
-    # if that column is full, play the next highest amplitude
-    i = 0
-    valid = False
-    while valid == False:
-      # take input from player (which column)
-      plays = bot_plays[i]
-      print(f"{bot.name} plays {plays}")
-      i += 1
-      # check if player's input is correct
-      valid = True
-      # 2. columns must not be full
-      if not "" in list(self.game[plays]):
-        valid = False
-        print("This column is full.")
-        continue
-
-      # depending on player, drop a character in the lowest empty row
-      play_col = list(self.game[plays])
-
-      done = False
-      i = -1
-      symbol = self.symbols[self.turn - 1]
-      while not done:
-        if play_col[i] == "":
-          play_col[i] = symbol
-          self.game[plays] = play_col
-          print(f"\nPlaced a {symbol} in column {plays}.\n")
-          done = True
-          return "play"
+        if hasattr(self, "winner"):
+            print(f"The winner is Player {self.winner}!")
+        elif self.status == "tie":
+            print(f"The game has ended in a tie!")
         else:
-          i -= 1
+            print(f"No winner yet.")
 
-  def ai_vs_ai(self, bot_1, bot_2, slow=False):
-    command = None
-    self.show()
-    if not hasattr(self, "player_names"):
-      pl1_name = bot_1.name
-      pl2_name = bot_2.name
-      self.player_names = [pl1_name, pl2_name]
-    while self.check_winner() == False and command != "stop":
-      if slow:
-        command = input("Enter 'stop' to stop. Enter anything or nothing to continue")
-      self.convert()
+    def reset(self):
+        self.game = pd.DataFrame(data="",
+                                 index=self.game_rows,
+                                 columns=self.game_cols)
+        if hasattr(self, "winner"):
+            del (self.winner)
+        del (self.player_names)
+        self.turn = next(self.turns)
+        self.status = "start"
 
-      if self.turn == 1:
-        self.bot_turn(bot_1)
-      else:
-        self.bot_turn(bot_2)
+    def convert(self):
+        game = self.game.values
+        shape = game.shape
+        converted_game = np.zeros(shape)
+        for x in range(0, shape[0]):
+            for y in range(0, shape[1]):
+                if game[x, y] == "":
+                    converted_game[x, y] = 0
+                elif game[x, y] == "x":
+                    converted_game[x, y] = 1
+                elif game[x, y] == "o":
+                    converted_game[x, y] = -1
+        self.conv_game = converted_game.reshape(42, 1)
 
-      self.show()
-      self.turn = next(self.turns)
+    def play_vs_ai(self, bot):
+        command = None
+        print("We are now playing CONNECT 4!!!")
+        self.show()
+        if not hasattr(self, "player_names"):
+            pl1_name = input("Player 1, what is your name?\n")
+            pl2_name = bot.name
+            self.player_names = [pl1_name, pl2_name]
+        while self.check_winner() == False and command != "stop":
+            if self.turn == 1:
+                command = self.take_turn()
+            else:
+                self.bot_turn(bot)
 
-    if hasattr(self, "winner"):
-      print(f"The winner is {self.winner}!")
-    elif self.status == "tie":
-      print(f"The game has ended in a tie!")
-    else:
-      print(f"No winner yet.")
+            self.convert()
+            self.show()
+            self.turn = next(self.turns)
 
+        if hasattr(self, "winner"):
+            print(f"The winner is {self.winner}!")
+        elif self.status == "tie":
+            print(f"The game has ended in a tie!")
+        else:
+            print(f"No winner yet.")
+
+    def bot_turn(self, bot):
+        game_inputs = self.conv_game
+        if self.turn == 2:
+            game_inputs = [-x for x in game_inputs]
+        # let the bot calculate his turn
+        bot_activation = list(bot.calculate(game_inputs))
+        # play the column with the highest amplitude
+        #print(bot_activation)
+        bot_plays = [int(bot_activation.index(x)) + 1 for x in sorted(bot_activation, reverse=True)]
+        # if that column is full, play the next highest amplitude
+        i = 0
+        valid = False
+        while valid == False:
+            # take input from player (which column)
+            plays = bot_plays[i]
+            print(f"{bot.name} plays {plays}")
+            i += 1
+            # check if player's input is correct
+            valid = True
+            # 2. columns must not be full
+            if "" not in list(self.game[plays]):
+                valid = False
+                print("This column is full.")
+                continue
+
+            # depending on player, drop a character in the lowest empty row
+            play_col = list(self.game[plays])
+
+            done = False
+            i = -1
+            symbol = self.symbols[self.turn - 1]
+            while not done:
+                if play_col[i] == "":
+                    play_col[i] = symbol
+                    self.game[plays] = play_col
+                    print(f"\nPlaced a {symbol} in column {plays}.\n")
+                    done = True
+                    return "play"
+                else:
+                    i -= 1
+
+    def ai_vs_ai(self, bot_1, bot_2, slow=False):
+        command = None
+        self.show()
+        if not hasattr(self, "player_names"):
+            pl1_name = bot_1.name
+            pl2_name = bot_2.name
+            self.player_names = [pl1_name, pl2_name]
+        while self.check_winner() == False and command != "stop":
+            if slow:
+                command = input("Enter 'stop' to stop. Enter anything or nothing to continue")
+            self.convert()
+
+            if self.turn == 1:
+                self.bot_turn(bot_1)
+            else:
+                self.bot_turn(bot_2)
+
+            self.show()
+            self.turn = next(self.turns)
+
+        if hasattr(self, "winner"):
+            print(f"The winner is {self.winner}!")
+        elif self.status == "tie":
+            print(f"The game has ended in a tie!")
+        else:
+            print(f"No winner yet.")
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import os
 import pickle
+import time
+from itertools import combinations
 from neuralnetwork import *
 from connect4 import *
 import numpy as np
+import pandas as pd
 
 with open("Names.txt", "r") as f:
     names = f.readlines()
@@ -9,20 +13,207 @@ with open("Names.txt", "r") as f:
 names = [x[:-1] for x in names]
 
 
-def random_name(names):
-    name = np.random.choice(names)
+def random_name(names_list):
+    name = np.random.choice(names_list)
     return name
 
 
-def new_generation(size=10):
+def new_generation(names_list, size=10, bot_df: pd.DataFrame = None):
     bot_list = []
+    first_gen = False
 
     for i in range(size):
         new_bot = GamBot()
-        new_bot.name = random_name(names)
+        new_bot.name = random_name(names_list)
         bot_list.append(new_bot)
 
-    return bot_list
+    if not bot_df:
+        bot_df = pd.DataFrame()
+        gen = 1
+        first_gen = True
+    else:
+        gen = max(bot_df["gen"]+1)
+
+    for bot in bot_list:
+        # check if name is already in bot_df
+        if first_gen:
+            new_id = 0
+            first_gen = False
+        else:
+            new_id = max(bot_df.index) + 1
+
+        bot.name = f"{bot.name} #{new_id:05d}"
+        bot_dict = dict(gen=gen, parent_1=None, parent_2=None,
+                        bot=bot, status="alive", wins=0, loses=0, w_per_l=0, plays=0, score=0, max_score=0)
+        bot_df = bot_df.append(bot_dict, ignore_index=True)
+
+    return bot_df
+
+
+def tournament(bot_df, hist_df=None):
+    tour_df = bot_df[bot_df["status"] == "alive"]
+    games = list(combinations(tour_df.index, 2))
+
+    if not isinstance(hist_df, pd.DataFrame):
+        hist_df = pd.DataFrame()
+        round = 1
+    else:
+        round = max(hist_df["round"])+1
+
+    for game in games:
+        # get bots
+        bot_1 = tour_df.at[game[0], "bot"]
+        bot_2 = tour_df.at[game[1], "bot"]
+
+        # generate a connect4 instance
+        con4 = ConnectFour(f"{bot_1.name} vs {bot_2.name}")
+
+        # let the bots play against each other
+        print(f"{bot_1.name} is playing against {bot_2.name}!")
+        playtime = time.ctime(time.time())
+        con4.ai_vs_ai(bot_1, bot_2)
+
+        # write down the winner or, if there is a tie, write down tie
+        if con4.status == "finished":
+            winner = con4.winner
+            winner_name = bot_1.name if winner == 1 else bot_2.name
+            print(f"{winner_name} has won!")
+        elif con4.status == "tie":
+            winner, winner_name = None, None
+            print(f"Wow! The game ended in a tie!")
+        else:
+            print("Something must have gone wrong...")
+            winner, winner_name = None, None
+
+        # write important data about the game to dict
+        game_dict = dict(p1=game[0], p2=game[1], p1_name=bot_1.name, p2_name=bot_2.name, time=playtime, winner=winner,
+                         round=round)
+
+        # write dict to hist_df
+        hist_df = hist_df.append(game_dict, ignore_index=True)
+
+        hist_df["p1"] = [int(x) for x in hist_df["p1"]]
+        hist_df["p2"] = [int(x) for x in hist_df["p2"]]
+        hist_df["winner"] = [int(x) for x in hist_df["winner"]]
+
+    return hist_df
+
+
+def save_bots(bot_df, filename, overwrite: bool = False):
+    """
+    Function for quickly pickling bot dataframes
+    :param overwrite: bool to determine whether file shall be overwritten
+    :param bot_df: DataFrame containing GamBots and data on them
+    :param filename: string name for filename
+    :return: does not return anything
+    """
+    if os.path.isfile(f"bot_library\\{filename}.txt") and overwrite == False:
+        user = input("This file exists already. Continue? y/n")
+        if user != "y":
+            print("Aborting save.")
+            return False
+
+    with open(f"bot_library\\{filename}.txt", "wb") as my_file:
+        pickle.dump(bot_df, my_file)
+        print("bot_df saved to file.")
+
+    return True
+
+
+def load_bots(filename):
+    if not os.path.isfile(f"bot_library\\{filename}.txt"):
+        raise ValueError(f"File {filename}.txt not found!")
+
+    else:
+        with open(f"bot_library\\{filename}.txt", "rb") as my_file:
+            bot_df = pickle.load(my_file)
+
+    return bot_df
+
+
+def calculate_scores(bot_df, hist_df):
+    """
+    Function to calculate scores from hist_df and update the values in bot_df
+    :param bot_df:
+    :param hist_df:
+    :return:
+    """
+    current_round = hist_df[hist_df["round"]==max(hist_df["round"])]
+    for row in current_round.index:
+        p1 = hist_df.at[row, "p1"]
+        p2 = hist_df.at[row, "p2"]
+        bot_df.at[p1, "plays"] += 1
+        bot_df.at[p2, "plays"] += 1
+
+        game_winner = hist_df.at[row, "winner"]
+        if game_winner is None:
+            bot_df.at[p1, "score"] += 1
+            bot_df.at[p2, "score"] += 1
+
+            continue
+        elif game_winner == 1:
+            bot_df.at[p1, "score"] += 3
+            bot_df.at[p1, "wins"] += 1
+            bot_df.at[p2, "loses"] += 1
+            continue
+        elif game_winner == 2:
+            bot_df.at[p2, "score"] += 3
+            bot_df.at[p2, "wins"] += 1
+            bot_df.at[p1, "loses"] += 1
+            continue
+
+        else:
+            print("What the hell is even that.")
+
+    for row in bot_df.index:
+        if bot_df.at[row, "loses"] == 0:
+            bot_df.at[row, "w_per_l"] = np.inf
+        else:
+            bot_df.at[row, "w_per_l"] = bot_df.at[row, "wins"] / bot_df.at[row, "loses"]
+
+    bot_df = bot_df.sort_values(by="w_per_l", ascending=False)
+
+    for row in bot_df.index:
+        bot_df.at[row, "max_score"] = max((bot_df.at[row, "score"], bot_df.at[row, "max_score"]))
+
+    return bot_df
+
+
+def procreate(bot_df, procreation_quantity=10, names_list=None):
+    if names_list is None:
+        names_list = names
+    bot_df = bot_df.sort_values(by="score", ascending=False)
+    bot_df["status"] = "deceased"
+    id_to_procreate = list(bot_df.head(procreation_quantity).index)
+    for survivor in id_to_procreate:
+        bot_df.loc[survivor, "status"] = "alive"
+
+    to_procreate = list(bot_df.head(procreation_quantity)["bot"])
+    # generate randomized coupling from procreation quantity
+    # make sure that each parent gets two children
+    np.random.shuffle(to_procreate)
+    couples = [(to_procreate[i-1], to_procreate[i]) for i in range(len(to_procreate))]
+
+    gen = max(bot_df["gen"])+1
+
+    # loop through couples
+    for couple in couples:
+
+        # cross the parents
+        child = couple[0].cross(couple[1])
+        # mutate the child
+        child.full_mutate()
+        # generate new dictionary
+
+        new_id = max(bot_df.index) + 1
+        name = random_name(names_list)
+        child.name = f"{name} #{new_id:05d}"
+        child_dict = dict(gen=gen, parent_1=couple[0].name, parent_2=couple[1].name,
+                        bot=child, status="alive", wins=0, loses=0, w_per_l=0, plays=0, score=0, max_score=0)
+
+        bot_df = bot_df.append(child_dict, ignore_index=True)
+
+    return bot_df
 
 """
 Start procedure here
